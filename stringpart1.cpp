752712e52a34beb4023f2600a736769785f6031e
Reeshu singh <114919870+Reeshusingh29@users.noreply.github.com>
2023-06-21 22:27:22 +0530

    Add files via upload

---
+++ stringpart1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	/*string str="Reeshu singh";
+	string str1("BABU");
+	 cout<<str<<" "<<str1<<endl;
+	*/
+	/*string str;
+	cin>>str;
+	cout<<str;
+	
+	//To over come this spacing problem we use getline 
+	string str;
+	getline(cin,str);
+	cout<<str<<endl;
+	
+	//indexing concept in string 
+	string str="Babu";
+	//ASCII CONCEPT IN STRING 
+	char ch='A';
+	cout<<int(ch)<<endl;
+	//cout<<str[3]<<endl; 
+	*/
+	string str="HELLO";
+	//reverse(str.begin(),str.end());
+	//substr() function concept
+	cout<<str.substr(0,3)<<endl;
+	
+	return 0;
+}
